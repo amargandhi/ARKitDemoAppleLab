871d799c7f8280a790cd5cd322b36a6e62817bbd
Amar Gandhi <amar.gandhi@me.com>
2024-04-24 11:31:50 +0100

    Initial Commit

---
+++ ARKit Demo/ARKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKit_DemoApp.swift
+//  ARKit Demo
+//
+//  Created by Amar Gandhi on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ARKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
